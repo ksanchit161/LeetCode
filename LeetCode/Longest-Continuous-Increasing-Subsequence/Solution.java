d5e2e71f738ed479f234420e1af2649fed6c431b
Sanchit Garg <ksanchit161@gmail.com>
2026-07-23 02:46:19 +0530

    Add solution for Longest Continuous Increasing Subsequence

--- LeetCode/Longest-Continuous-Increasing-Subsequence/Solution.java
+++ LeetCode/Longest-Continuous-Increasing-Subsequence/Solution.java
@@ -1,12 +1,18 @@
 1class Solution {
-2    public int findLengthOfLCIS(int[] nums) {
-3        int max=1;
-4        int length=1;
-5        for(int i=0;i<nums.length-1;i++){
-6            if(nums[i]<nums[i+1]) length+=1;
-7            if(length>max) max=length;
-8            if(nums[i]>=nums[i+1]) length=1;
-9        }
-10        return max;
-11    }
-12}
+2    static{
+3       int number[]={1,2,4,3};
+4        for(int i=0;i<100;i++){
+5             findLengthOfLCIS(number);
+6        }
+7    }
+8    public static int findLengthOfLCIS(int[] nums) {
+9        int max=1;
+10        int length=1;
+11        for(int i=0;i<nums.length-1;i++){
+12            if(nums[i]<nums[i+1]) length+=1;
+13            if(length>max) max=length;
+14            if(nums[i]>=nums[i+1]) length=1;
+15        }
+16        return max;
+17    }
+18}
