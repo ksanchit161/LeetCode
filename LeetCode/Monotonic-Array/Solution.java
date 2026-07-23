c5f9e97a84fe98644bcd0392ce6ed5db76667e82
Sanchit Garg <ksanchit161@gmail.com>
2026-07-23 06:58:19 +0530

    Add solution for Monotonic Array

--- LeetCode/Monotonic-Array/Solution.java
+++ LeetCode/Monotonic-Array/Solution.java
@@ -1,12 +1,18 @@
 1class Solution {
-2    public boolean isMonotonic(int[] nums) {
-3        int inc=0;
-4        int dec=0;
-5        for(int i=0;i<nums.length-1;i++){
-6            if(nums[i]<nums[i+1]) inc++;
-7            else if(nums[i]>nums[i+1]) dec++;
-8            if(inc>0 && dec>0) return false;
-9        }
-10        return true;
-11    }
-12}
+2    static{
+3        int []numbers={1,2,3,5};
+4        for(int i=0;i<1000;i++){
+5             isMonotonic(numbers);
+6        }
+7    }
+8    public static boolean isMonotonic(int[] nums) {
+9        int inc=0;
+10        int dec=0;
+11        for(int i=0;i<nums.length-1;i++){
+12            if(nums[i]<nums[i+1]) inc++;
+13            else if(nums[i]>nums[i+1]) dec++;
+14            if(inc>0 && dec>0) return false;
+15        }
+16        return true;
+17    }
+18}
