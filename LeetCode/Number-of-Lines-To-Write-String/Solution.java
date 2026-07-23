8e7eed5e711aeaeb6c40604ca739487f53c1a62c
Sanchit Garg <ksanchit161@gmail.com>
2026-07-23 05:34:59 +0530

    Add solution for Number of Lines To Write String

--- LeetCode/Number-of-Lines-To-Write-String/Solution.java
+++ LeetCode/Number-of-Lines-To-Write-String/Solution.java
@@ -1,20 +1,26 @@
 1class Solution {
-2    public int[] numberOfLines(int[] widths, String s) {
-3        int []result=new int [2];
-4        int lines=0;
-5        int width=0;
-6        for(int i=0;i<s.length();i++){
-7            int len=widths[s.charAt(i)-'a'];
-8            if((width+len)<=100) width+=len;
-9            else{
-10                lines+=1;
-11                width=len;
-12            }
-13
-14        }
-15        if(width>0) lines+=1;
-16        result[0]=lines;
-17        result[1]=width;
-18        return result;
-19    }
-20}
+2    static{
+3        int []number={10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10};
+4        for(int i=0;i<=100;i++){
+5             numberOfLines(number,"abcedfegheh");
+6        }
+7    }
+8    public static int[] numberOfLines(int[] widths, String s) {
+9        int []result=new int [2];
+10        int lines=0;
+11        int width=0;
+12        for(int i=0;i<s.length();i++){
+13            int len=widths[s.charAt(i)-'a'];
+14            if((width+len)<=100) width+=len;
+15            else{
+16                lines+=1;
+17                width=len;
+18            }
+19
+20        }
+21        if(width>0) lines+=1;
+22        result[0]=lines;
+23        result[1]=width;
+24        return result;
+25    }
+26}
