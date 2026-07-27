8a800991cf2ed7283a07463afae2810a01478bdc
Sanchit Garg <ksanchit161@gmail.com>
2026-07-28 01:30:27 +0530

    Add solution for Find Missing Elements

--- LeetCode/Find-Missing-Elements/Solution.java
+++ LeetCode/Find-Missing-Elements/Solution.java
@@ -1,19 +1,25 @@
 1class Solution {
-2    public List<Integer> findMissingElements(int[] nums) {
-3        int min=101;
-4        int max=-1;
-5        for(int i=0;i<nums.length;i++){
-6            min=Math.min(min,nums[i]);
-7            max=Math.max(max,nums[i]);
-8        }
-9        int arr[]=new int[max-min+1];
-10        for(int ele:nums){
-11            arr[ele-min]+=1;
-12        }
-13        List<Integer> result=new ArrayList<>();
-14        for(int i=0;i<=max-min;i++){
-15            if(arr[i]==0) result.add(i+min);
-16        }
-17        return result;
-18    }
-19}
+2    static{
+3       int [] number={1,2,3,4,5};
+4        for(int i=0;i<1000;i++){
+5            findMissingElements(number);
+6        }
+7    }
+8    public static  List<Integer> findMissingElements(int[] nums) {
+9        int min=101;
+10        int max=-1;
+11        for(int i=0;i<nums.length;i++){
+12            min=Math.min(min,nums[i]);
+13            max=Math.max(max,nums[i]);
+14        }
+15        int arr[]=new int[max-min+1];
+16        for(int ele:nums){
+17            arr[ele-min]+=1;
+18        }
+19        List<Integer> result=new ArrayList<>();
+20        for(int i=0;i<=max-min;i++){
+21            if(arr[i]==0) result.add(i+min);
+22        }
+23        return result;
+24    }
+25}
