15eaaaa35a0c2069a13e1fe2c2442eaa86a6f441
Sanchit Garg <ksanchit161@gmail.com>
2026-07-27 22:50:11 +0530

    Add solution for Maximum Product of Two Elements in an Array

--- LeetCode/Maximum-Product-of-Two-Elements-in-an-Array/Solution.java
+++ LeetCode/Maximum-Product-of-Two-Elements-in-an-Array/Solution.java
@@ -1,12 +1,16 @@
 1class Solution {
 2    public int maxProduct(int[] nums) {
-3    PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
-4    for(int ele: nums){
-5        pq.add(ele);
-6    }
-7    int first=pq.poll()-1;
-8    int second=pq.poll()-1;
-9    return first*second;
-10
-11    }
-12}
+3        int max=0;
+4        int smax=-1;
+5        for(int i=0;i<nums.length;i++){
+6            if(nums[i]>max){
+7                smax=max;
+8                max=nums[i];
+9            }
+10            else if(nums[i]>smax){
+11                smax=nums[i];
+12            }
+13        }
+14        return (max-1)*(smax-1);
+15    }
+16}
