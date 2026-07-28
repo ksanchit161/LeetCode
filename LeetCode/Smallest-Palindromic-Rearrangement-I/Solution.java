cc233a205b14badc7156ee3dd8f9a73c07f5eeec
Sanchit Garg <ksanchit161@gmail.com>
2026-07-28 18:04:08 +0530

    Add solution for Smallest Palindromic Rearrangement I

---
+++ LeetCode/Smallest-Palindromic-Rearrangement-I/Solution.java
@@ -0,0 +1,25 @@
+1class Solution {
+2    public String smallestPalindrome(String s) {
+3        if(s.length()==1) return s;
+4        int arr[]=new int[26];
+5        StringBuilder sb1=new StringBuilder("");
+6        StringBuilder sb2=new StringBuilder("");
+7        char mid='\0';
+8        for(int i=0;i<s.length();i++){
+9            int idx=s.charAt(i)-'a';
+10            arr[idx]++;
+11        }
+12        for(int i=0;i<26;i++){
+13            char ch=(char)(i+97);
+14            while(arr[i]>=2){
+15                sb1.append(ch);
+16                sb2.append(ch);
+17                arr[i]-=2;
+18            }
+19            if(arr[i]==1)mid=ch;
+20        }
+21        if(mid!='\0') sb1.append(mid);
+22        return sb1.toString()+sb2.reverse().toString();
+23
+24    }
+25}
