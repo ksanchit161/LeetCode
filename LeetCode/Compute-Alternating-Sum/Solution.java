d3f179c10b8279039ddce92943d7fafe401ae10f
Sanchit Garg <ksanchit161@gmail.com>
2026-07-29 22:56:39 +0530

    Add solution for Compute Alternating Sum

--- LeetCode/Compute-Alternating-Sum/Solution.java
+++ LeetCode/Compute-Alternating-Sum/Solution.java
@@ -1,11 +1,10 @@
 1class Solution {
 2    public int alternatingSum(int[] nums) {
-3        int even=0;
-4        int odd=0;
-5        for(int i=0;i<nums.length;i++){
-6            if(i%2==0) even+=nums[i];
-7            else odd+=nums[i];
-8        }
-9        return even-odd;
-10    }
-11}
+3        int sum=0;
+4        for(int i=0;i<nums.length;i++){
+5            if(i%2==0) sum+=nums[i];
+6            else sum-=nums[i];
+7        }
+8        return sum;
+9    }
+10}
