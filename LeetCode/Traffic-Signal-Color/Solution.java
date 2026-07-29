725c0f702a2b85e9bfb142d739c6312f2d1e1922
Sanchit Garg <ksanchit161@gmail.com>
2026-07-29 19:56:04 +0530

    Add solution for Traffic Signal Color

---
+++ LeetCode/Traffic-Signal-Color/Solution.java
@@ -0,0 +1,8 @@
+1class Solution {
+2    public String trafficSignal(int timer) {
+3        if(timer==0) return "Green";
+4        else if(timer==30) return "Orange";
+5        else if(timer>30 && timer<=90) return "Red";
+6        else return "Invalid";
+7    }
+8}
