08519b88c7b3e45278c3026a2524418edafc6fed
Sanchit Garg <ksanchit161@gmail.com>
2026-07-29 23:03:47 +0530

    Add solution for Digit Frequency Score

--- LeetCode/Digit-Frequency-Score/Solution.java
+++ LeetCode/Digit-Frequency-Score/Solution.java
@@ -1,15 +1,11 @@
 1class Solution {
 2    public int digitFrequencyScore(int n) {
-3        int arr[]=new int[10];
+3        int sum=0;
 4        while(n>0){
 5            int last=n%10;
-6            arr[last]+=1;
+6            sum+=last;
 7            n/=10;
 8        }
-9        int sum=0;
-10        for(int i=1;i<10;i++){
-11            sum+=arr[i]*i;
-12        }
-13        return sum;
-14    }
-15}
+9        return sum;
+10    }
+11}
