5bdd71ec3391533723fea517311fe098e047ac5a
Sanchit Garg <ksanchit161@gmail.com>
2026-07-30 00:10:58 +0530

    Add solution for DI String Match

--- LeetCode/DI-String-Match/Solution.java
+++ LeetCode/DI-String-Match/Solution.java
@@ -4,11 +4,13 @@
 4        int j=s.length();
 5        int []result=new int [j+1];
 6        int k=0;
-7        for(int l=0;l<s.length();l++){
-8            if(s.charAt(l)=='I') result[k++]=i++;
-9            else result[k++]=j--;
-10        }
-11        result[k]=i;
-12        return result;
-13    }
-14}
+7        char[] ch = s.toCharArray();
+8
+9        for(int l=0;l<ch.length;l++){
+10            if(ch[l]=='I') result[k++]=i++;
+11            else result[k++]=j--;
+12        }
+13        result[k]=i;
+14        return result;
+15    }
+16}
