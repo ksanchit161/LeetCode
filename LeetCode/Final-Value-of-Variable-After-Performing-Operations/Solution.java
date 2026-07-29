23990fbae475700155d45870982a1d120d0f08c4
Sanchit Garg <ksanchit161@gmail.com>
2026-07-29 20:32:14 +0530

    Add solution for Final Value of Variable After Performing Operations

--- LeetCode/Final-Value-of-Variable-After-Performing-Operations/Solution.java
+++ LeetCode/Final-Value-of-Variable-After-Performing-Operations/Solution.java
@@ -2,7 +2,7 @@
 2    public int finalValueAfterOperations(String[] operations) {
 3        int x=0;
 4        for(int i=0;i<operations.length;i++){
-5            if((operations[i].equals("--X")) || (operations[i].equals("X--"))) x-=1;
+5            if(operations[i].charAt(1)=='-') x-=1;
 6            else x+=1;
 7        }
 8        return x;
