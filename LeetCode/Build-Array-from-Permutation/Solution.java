175f61bc06c26c850297c3957101c2d019b64acb
Sanchit Garg <ksanchit161@gmail.com>
2026-07-29 20:21:30 +0530

    Add solution for Build Array from Permutation

---
+++ LeetCode/Build-Array-from-Permutation/Solution.java
@@ -0,0 +1,9 @@
+1class Solution {
+2    public int[] buildArray(int[] nums) {
+3        int[]ans=new int [nums.length];
+4        for(int i=0;i<nums.length;i++){
+5            ans[i]=nums[nums[i]];
+6        }
+7        return ans;
+8}
+9    }
