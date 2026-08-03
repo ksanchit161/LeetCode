11ea3ec2d76755a14291393c1ef0e94cb95d936c
Sanchit Garg <ksanchit161@gmail.com>
2026-08-03 20:22:26 +0530

    Add solution for Array Partition

--- LeetCode/Array-Partition/Solution.java
+++ LeetCode/Array-Partition/Solution.java
@@ -1,10 +1,32 @@
 1class Solution {
 2    public int arrayPairSum(int[] nums) {
-3        Arrays.sort(nums);
-4        int sum=0;
-5        for(int i=0;i<nums.length;i+=2){
-6            sum+=Math.min(nums[i],nums[i+1]);
-7        }
-8        return sum;
-9    }
-10}
+3        int len=nums.length;
+4        int arr[]=new int[20001];
+5        int max=-1;
+6        for(int ele:nums){
+7            arr[ele+10000]+=1;
+8            ele+=10000;
+9            if(ele>max) max=ele;
+10        }
+11        int sum=0;
+12        int count=0;
+13        int i=0;
+14       while(i<=max){
+15            while(arr[i]>=2 && count==0){
+16                sum+=i;
+17                arr[i]-=2;
+18            }
+19            if(arr[i]>=1 && count==0){
+20                sum+=i;
+21                arr[i]-=1;
+22                count+=1;
+23            }
+24            else if(arr[i]>=1 && count==1){
+25                arr[i]-=1;
+26                count=0;
+27            }
+28            else if(arr[i]==0) i++;
+29        }
+30        return sum-(len/2)*10000;
+31    }
+32}
