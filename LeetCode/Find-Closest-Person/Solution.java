7aa5eed3ee2e24e25030b35669b70614d40dc6a9
Sanchit Garg <ksanchit161@gmail.com>
2026-08-06 00:09:36 +0530

    Add solution for Find Closest Person

--- LeetCode/Find-Closest-Person/Solution.java
+++ LeetCode/Find-Closest-Person/Solution.java
@@ -1,12 +1,10 @@
 1class Solution {
 2    public int findClosest(int x, int y, int z) {
-3        int d1=z-x;
-4        int d2=z-y;
-5        if(d1<0) d1*=-1;
-6        if(d2<0) d2*=-1;
-7        if(d1==d2) return 0;
-8        else if(d1<d2) return 1;
-9        else return 2;
-10
-11    }
-12}
+3        int d1=Math.abs(z-x);
+4        int d2=Math.abs(z-y);
+5        if(d1==d2) return 0;
+6        else if(d1<d2) return 1;
+7        else return 2;
+8
+9    }
+10}
