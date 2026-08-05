73cb5c33431176fb1e2b8b09282f80b43eb787d6
Sanchit Garg <ksanchit161@gmail.com>
2026-08-05 23:03:19 +0530

    Add solution for Transform Array by Parity

---
+++ LeetCode/Transform-Array-by-Parity/Solution.java
@@ -0,0 +1,14 @@
+1class Solution {
+2    public int[] transformArray(int[] nums) {
+3        int count=0;
+4        int res[]=new int[nums.length];
+5        for(int ele:nums){
+6            if(ele%2==0) count++;
+7        }
+8        for(int i=count;i<nums.length;i++){
+9            res[i]=1;
+10        }
+11        return res;
+12        
+13    }
+14}
