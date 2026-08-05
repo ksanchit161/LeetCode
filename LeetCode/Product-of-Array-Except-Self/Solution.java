54d9100ca50d3ea07f7b7b960f9c900ae866916e
Sanchit Garg <ksanchit161@gmail.com>
2026-08-05 20:35:09 +0530

    Add solution for Product of Array Except Self

--- LeetCode/Product-of-Array-Except-Self/Solution.java
+++ LeetCode/Product-of-Array-Except-Self/Solution.java
@@ -1,28 +1,17 @@
 1class Solution {
 2    public int[] productExceptSelf(int[] nums) {
-3        int zero=0;
-4        int id=-1;
-5        int prod=1;
-6        int res[]=new int[nums.length];
-7        for(int i=0;i<nums.length;i++){
-8            if(nums[i]==0){
-9                zero++;
-10                id=i;
-11            }
-12            else prod*=nums[i];
+3        int left=1;
+4        int right=1;
+5        int res[]=new int[nums.length];
+6        for(int i=0;i<nums.length;i++){
+7            res[i]=left;
+8            left*=nums[i];
+9        }
+10        for(int i=nums.length-1;i>=0;i--){
+11            res[i]*=right;
+12            right*=nums[i];
 13        }
-14        if(zero>=2){
-15            return res;
-16        }
-17        else if(zero>=1){
-18            res[id]=prod;
-19            return res;
-20        }
-21        else if(zero==0){
-22            for(int i=0;i<nums.length;i++){
-23                res[i]=prod/nums[i];
-24            }
-25        }
-26        return res;
-27    }
-28}
+14        return res;
+15    }
+16
+17}
