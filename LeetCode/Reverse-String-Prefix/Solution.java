bd98e947bd38fd07986411c27e7e7a4282bf01f8
Sanchit Garg <ksanchit161@gmail.com>
2026-08-05 23:50:24 +0530

    Add solution for Reverse String Prefix

--- LeetCode/Reverse-String-Prefix/Solution.java
+++ LeetCode/Reverse-String-Prefix/Solution.java
@@ -1,9 +1,15 @@
 1class Solution {
 2    public String reversePrefix(String s, int k) {
-3        var temp=new StringBuilder("");
-4        temp.append(s.substring(0,k));
-5        temp.reverse();
-6        temp.append(s.substring(k,s.length()));
-7        return temp.toString();
-8    }
-9}
+3        char [] arr=s.toCharArray();
+4        int i=0;
+5        int j=k-1;
+6        while(i<=j){
+7            char temp=arr[i];
+8            arr[i]=arr[j];
+9            arr[j]=temp;
+10            i++;
+11            j--;
+12        }
+13        return String.valueOf(arr);
+14    }
+15}
