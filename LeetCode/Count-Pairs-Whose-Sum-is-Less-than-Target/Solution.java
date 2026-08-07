fdf51e24ef88ea2302c90afb23a983de078af34e
Sanchit Garg <ksanchit161@gmail.com>
2026-08-08 01:01:18 +0530

    Add solution for Count Pairs Whose Sum is Less than Target

--- LeetCode/Count-Pairs-Whose-Sum-is-Less-than-Target/Solution.java
+++ LeetCode/Count-Pairs-Whose-Sum-is-Less-than-Target/Solution.java
@@ -1,23 +1,17 @@
 1class Solution {
-2  static {
-3        for (int i = 0; i < 1000; i++) {
-4            List<Integer> dummy = new ArrayList<>(Arrays.asList(1, 2, 3));
-5            countPairs(dummy, 5);
-6        }
-7    }
-8    public static  int countPairs(List<Integer> nums, int target) {
-9      Collections.sort(nums);
-10        int i=0;
-11        int j=nums.size()-1;
-12        int result=0;
-13        while(i<j){
-14            int sum=nums.get(i)+nums.get(j);
-15            if(sum>=target) j--;
-16            else{
-17                result+=(j-i);
-18                i++;
-19            }
-20        }
-21        return result;
-22    }
-23}
+2    public  int countPairs(List<Integer> nums, int target) {
+3      Collections.sort(nums);
+4        int i=0;
+5        int j=nums.size()-1;
+6        int result=0;
+7        while(i<j){
+8            int sum=nums.get(i)+nums.get(j);
+9            if(sum>=target) j--;
+10            else{
+11                result+=(j-i);
+12                i++;
+13            }
+14        }
+15        return result;
+16    }
+17}
