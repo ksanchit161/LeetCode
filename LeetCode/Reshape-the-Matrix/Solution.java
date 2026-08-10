65120f57e8ccd6abf8a2fa62f25532f62cd1eafc
Sanchit Garg <ksanchit161@gmail.com>
2026-08-10 21:05:57 +0530

    Add solution for Reshape the Matrix

--- LeetCode/Reshape-the-Matrix/Solution.java
+++ LeetCode/Reshape-the-Matrix/Solution.java
@@ -3,20 +3,14 @@
 3        int n=mat.length;
 4        int m=mat[0].length;
 5        if(m*n!=r*c) return mat;
-6        int temp[]=new int[m*n];
-7        int k=0;
-8        for(int i=0;i<n;i++){
-9            for(int j=0;j<m;j++){
-10                temp[k++]=mat[i][j];
-11            }
-12        }
-13        k=0;
-14        int result[][]=new int[r][c];
-15        for(int i=0;i<r;i++){
-16            for(int j=0;j<c;j++){
-17                result[i][j]=temp[k++];
-18            }
-19        }
-20        return result;
-21    }
-22}
+6        int k=0;
+7        int result[][]=new int[r][c];
+8        for(int i=0;i<r;i++){
+9            for(int j=0;j<c;j++){
+10                result[i][j] = mat[k/m][k%m];
+11                k++;
+12            }
+13        }
+14        return result;
+15    }
+16}
