82256c0f29e06fb1a69fc400ee4e2ac4881201c2
Sanchit Garg <ksanchit161@gmail.com>
2026-08-24 04:05:43 +0530

    Add solution for Count Equal and Divisible Pairs in an Array

--- LeetCode/Count-Equal-and-Divisible-Pairs-in-an-Array/Solution.java
+++ LeetCode/Count-Equal-and-Divisible-Pairs-in-an-Array/Solution.java
@@ -1,14 +1,16 @@
 1class Solution {
 2    public int countPairs(int[] nums, int k) {
-3      int n=nums.length,count=0;
-4      for(int i=0;i<n;i++){
-5        for(int j=i+1;j<n;j++){
-6            if(nums[j]==nums[i]){
-7                if((i*j)%k==0)
-8                count++;
-9            }
-10        }
-11      }
-12      return count;
-13    }
-14}
+3        int count=0;
+4        int n=nums.length;
+5        int i=0,j=1;
+6        while(i<j && i<n & j<n){
+7            if((i*j)%k==0 && nums[i]==nums[j]) count++;
+8            j++;
+9            if(j==n) {
+10                i++;
+11                j=i+1;
+12            }
+13        }
+14        return count;
+15    }
+16}
