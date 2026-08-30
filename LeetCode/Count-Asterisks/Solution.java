e552ecc8af457a32ca58e0be4037b892aa6ed223
Sanchit Garg <ksanchit161@gmail.com>
2026-08-30 22:10:01 +0530

    Add solution for Count Asterisks

--- LeetCode/Count-Asterisks/Solution.java
+++ LeetCode/Count-Asterisks/Solution.java
@@ -1,12 +1,16 @@
 1class Solution {
-2    public int countAsterisks(String s) {
-3        int count=0;
-4        int ans=0;
-5        for(int i=0;i<s.length();i++){
-6            char ch=s.charAt(i);
-7            if(ch=='|') count++;
-8            else if(count%2==0 && ch=='*') ans++;
-9        }
-10        return ans;
-11    }
-12}
+2     static {
+3        for(int i = 0; i < 500; i++)
+4            countAsterisks("");
+5    }
+6    public  static int countAsterisks(String s) {
+7        int count=0;
+8        int ans=0;
+9        for(int i=0;i<s.length();i++){
+10            char ch=s.charAt(i);
+11            if(ch=='|') count++;
+12            else if(count%2==0 && ch=='*') ans++;
+13        }
+14        return ans;
+15    }
+16}
