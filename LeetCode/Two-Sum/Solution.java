941af23a48a33626960236915497f2a95b40c78c
Sanchit Garg <ksanchit161@gmail.com>
2026-09-03 21:00:07 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.java
+++ LeetCode/Two-Sum/Solution.java
@@ -1,24 +1,17 @@
 1class Solution {
-2public:
-3    vector<int> twoSum(vector<int>& nums, int target) {
-4       vector<int> result=nums;
-5        vector<int> answer;
-6        sort(nums.begin(),nums.end());
-7        int start=0,end=nums.size()-1;
-8          while(start<end){
-9            if(nums[start]+nums[end]==target){
-10                start=nums[start];
-11                end=nums[end];
-12                break;
-13            }
-14            else if(nums[start]+nums[end]>target) end--;
-15            else start++;
-16        }
-17        for(int i=0;i<result.size();i++){
-18            if(result[i]==start) answer.push_back(i);
-19            else if(result[i]==end) answer.push_back(i);
-20        }
-21
-22            return answer;
-23    }
-24};
+2    public int[] twoSum(int[] nums, int target) {
+3        HashMap<Integer,Integer>map=new HashMap<>();
+4        int []result=new int[2];
+5        for(int i=0;i<nums.length;i++){
+6            int compliment=target-nums[i];
+7            if(map.containsKey(compliment)){
+8                result[0]=map.get(compliment);
+9                result[1]=i;
+10                return result;
+11            }
+12            map.put(nums[i],i);
+13            
+14        }
+15        return result;
+16    }
+17}
