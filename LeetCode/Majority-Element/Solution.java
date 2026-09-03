7cd16ce27412a7c3f6dc9a8797105c8d8a9e9444
Sanchit Garg <ksanchit161@gmail.com>
2026-09-03 21:05:14 +0530

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.java
+++ LeetCode/Majority-Element/Solution.java
@@ -1,22 +1,17 @@
 1class Solution {
 2    public int majorityElement(int[] nums) {
-3        int candidate = 0;
-4        int count = 0;
-5        
-6        for (int num : nums) {
-7            // If count falls to 0, pick a new candidate
-8            if (count == 0) {
-9                candidate = num;
-10            }
-11            
-12            // If it's our candidate, add a soldier. If not, lose a soldier.
-13            if (num == candidate) {
-14                count++;
-15            } else {
-16                count--;
-17            }
-18        }
-19        
-20        return candidate;
-21    }
-22}
+3        Arrays.sort(nums);
+4        int n=nums.length;
+5        int prev= nums[0];
+6        int count=1;
+7        for(int i=1;i<=n;i++){
+8            if(count>(n/2)) return prev;
+9            if(nums[i]==prev) count++;
+10            else {
+11                count=1;
+12                prev=nums[i];
+13            }
+14        }
+15        return prev;
+16    }
+17}
