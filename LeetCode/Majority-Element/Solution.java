c762f9ba80f47d1f2ee9ae655028930f89aaaef8
Sanchit Garg <ksanchit161@gmail.com>
2026-09-03 21:05:20 +0530

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.java
+++ LeetCode/Majority-Element/Solution.java
@@ -4,7 +4,7 @@
 4        int n=nums.length;
 5        int prev= nums[0];
 6        int count=1;
-7        for(int i=1;i<=n;i++){
+7        for(int i=1;i<n;i++){
 8            if(count>(n/2)) return prev;
 9            if(nums[i]==prev) count++;
 10            else {
