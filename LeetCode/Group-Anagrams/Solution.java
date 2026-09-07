54a549fe5cb01a70bf49ed0e9877f9e9f8a44639
Sanchit Garg <ksanchit161@gmail.com>
2026-09-07 20:33:05 +0530

    Add solution for Group Anagrams

--- LeetCode/Group-Anagrams/Solution.java
+++ LeetCode/Group-Anagrams/Solution.java
@@ -1,27 +1,23 @@
 1class Solution {
-2 public String sort(char[] brr) {
-3    int[] arr = new int[26];
-4    for (char ele : brr) {
-5        arr[ele - 'a']++;
-6    }
-7
-8    StringBuilder result = new StringBuilder(brr.length);
-9    for (int i = 0; i < 26; i++) {
-10        while (arr[i] > 0) {
-11            result.append((char) (i + 'a'));
-12            arr[i]--;
-13        }
-14    }
-15
-16    return result.toString();
-17}
-18    public List<List<String>> groupAnagrams(String[] strs) {
-19        HashMap<String,List<String>> map=new HashMap<>();
-20        for(String s: strs){
-21            char[] ca = s.toCharArray();                 
-22            String sorted = sort(ca);
-23            map.computeIfAbsent(sorted, k -> new ArrayList<>()).add(s);
-24        }
-25        return new ArrayList<>(map.values());
-26    }
-27}
+2    public List<List<String>> groupAnagrams(String[] strs) {
+3        Map<String, List<String>> map = new HashMap<>();
+4
+5        for (String s : strs) {
+6            int[] count = new int[26];
+7            for (int i = 0; i < s.length(); i++) {
+8                count[s.charAt(i) - 'a']++;
+9            }
+10
+11            // Create a fixed-format key, e.g., "#1#0#0#0#2..."
+12            StringBuilder sb = new StringBuilder();
+13            for (int c : count) {
+14                sb.append('#').append(c);
+15            }
+16            String key = sb.toString();
+17
+18            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+19        }
+20
+21        return new ArrayList<>(map.values());
+22    }
+23}
