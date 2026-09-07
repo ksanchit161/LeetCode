7b06d54ffdcc66b11bb1ec2e1e69065800aaa6e0
Sanchit Garg <ksanchit161@gmail.com>
2026-09-07 20:26:00 +0530

    Add solution for Group Anagrams

--- LeetCode/Group-Anagrams/Solution.java
+++ LeetCode/Group-Anagrams/Solution.java
@@ -1,13 +1,28 @@
 1class Solution {
-2    public List<List<String>> groupAnagrams(String[] strs) {
-3        HashMap<String,List<String>> map=new HashMap<>();
-4        for(int i=0;i<strs.length;i++){
-5            String temp=strs[i];
-6            char[] ca = strs[i].toCharArray(); 
-7            Arrays.sort(ca);                  
-8            String sorted = new String(ca);
-9            map.computeIfAbsent(sorted, k -> new ArrayList<>()).add(temp);
-10        }
-11        return new ArrayList<>(map.values());
-12    }
-13}
+2 public String sort(char[] brr) {
+3    int[] arr = new int[26];
+4    for (char ele : brr) {
+5        arr[ele - 'a']++;
+6    }
+7
+8    StringBuilder result = new StringBuilder(brr.length);
+9    for (int i = 0; i < 26; i++) {
+10        while (arr[i] > 0) {
+11            result.append((char) (i + 'a'));
+12            arr[i]--;
+13        }
+14    }
+15
+16    return result.toString();
+17}
+18    public List<List<String>> groupAnagrams(String[] strs) {
+19        HashMap<String,List<String>> map=new HashMap<>();
+20        for(int i=0;i<strs.length;i++){
+21            String temp=strs[i];
+22            char[] ca = strs[i].toCharArray();                 
+23            String sorted = sort(ca);
+24            map.computeIfAbsent(sorted, k -> new ArrayList<>()).add(temp);
+25        }
+26        return new ArrayList<>(map.values());
+27    }
+28}
