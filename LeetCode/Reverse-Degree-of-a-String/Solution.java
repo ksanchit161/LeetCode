9308aff186798c58ce1a84ec9aa748ea07d6e49f
Sanchit Garg <ksanchit161@gmail.com>
2026-09-20 18:23:57 +0530

    Add solution for Reverse Degree of a String

--- LeetCode/Reverse-Degree-of-a-String/Solution.java
+++ LeetCode/Reverse-Degree-of-a-String/Solution.java
@@ -1,12 +1,12 @@
 1class Solution {
 2    public int reverseDegree(String s) {
-3        char []arr=s.toCharArray();
-4        int sum=0;
+3        char arr[]=s.toCharArray();
+4        int ans=0;
 5        for(int i=0;i<arr.length;i++){
-6            int idx=(int)(arr[i]-'a');
-7            int prod=(i+1)*(26-idx);
-8            sum+=prod;
+6            char ch=arr[i];
+7            int idx=26-(int)(ch-'a');
+8            ans+=((i+1)*idx);
 9        }
-10        return sum;
+10        return ans;
 11    }
 12}
